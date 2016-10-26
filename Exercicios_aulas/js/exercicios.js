82f17a1378887cd97bb7f1cae635ccf24f17fa75
Ana Paula Rocha <ana.rocha@FEDUC.BR>
2016-10-26 10:02:45 -0200

    Exercicios de aula

---
+++ Exercicios_aulas/js/exercicios.js
@@ -0,0 +1,145 @@
+// Exercicio 1
+
+var v_um = 4;
+var v_dois = 10;
+
+v_um + v_dois
+
+var v_tres = 2;
+var v_quatro = 8;
+
+v_tres / v_quatro
+
+var v_cinco = 1;
+var v_seis = 6;
+
+v_cinco - v_seis
+
+// Exercicio 2
+
+3 > 5
+5 > 0 
+5 > 0
+5 > 5
+0 >= 1
+1 > -10
+
+3 > 5
+false
+5 > 0 
+true
+5 > 5
+false
+0 >= 1
+false
+1 > -10
+true
+
+// Exercicio 3
+
+(10 + 40) >= (50 - 10)
+1 > - 10
+(3 > 5) && (5 > 6)
+(5 < 0) && (0 == 0)
+( 5!= 4) || (4 > 4)
+
+(10 + 40) >= (50 - 10)
+true
+1 > - 10
+true
+(3 > 5) && (5 > 6)
+false
+(5 < 0) && (0 == 0)
+false
+( 5!= 4) || (4 > 4)
+true
+
+// Exercicio 4
+
+var Ana = 2;
+var Raquel = 4;
+if (Ana > Raquel) { console.log("é maior");
+} else { console.log("é menor"); }
+
+if (Ana > Raquel) 
+{ console.log("é maior");
+} else 
+{ console.log("é menor"); 
+}
+VM230:4 é menor
+
+// Exercicio 5
+
+var var_um = 1
+var var_dois = 1
+
+if (var_um > var_dois) 
+{ console.log("é maior");
+} else 
+{ console.log("é menor"); 
+}
+
+if (var_um > var_dois) 
+{ console.log("é maior"); }
+else if (var_um == var_dois)
+{ console.log("igual")}
+else
+{ console.log("é menor")};
+
+if (var_um > var_dois) 
+{ console.log("é maior"); }
+else if (var_um == var_dois)
+{ console.log("igual")}
+else
+{ console.log("é menor")};
+
+VM313:4 igual
+
+// Exercicio 6
+
+var var_um = prompt("1o num")
+var var_dois = prompt("2o num")
+
+alert(var_um * var_dois);
+
+
+// Exercicio 7
+
+var idade = prompt("Qual a sua idade?");
+ 
+if (idade > 18)
+{ 
+    alert("Você já pode dirigir")
+}else{ 
+    alert("Espere um pouco mais para dirigir")
+}
+
+// Exercicio 8
+
+var nome = prompt("Qual o seu nome?*")
+var sexo = prompt("Qual o seu sexo?")
+
+if (sexo == "feminino")
+{ 
+    alert("Bem-vinda " + nome);
+}
+else if (sexo == "masculino")
+{ 
+    alert("Bem-vindo " + nome);
+}
+else
+{ alert("Olá " + nome)}
+
+
+// Exercicio 9
+
+var horas = prompt("Que horas são?")
+
+if (horas >= 19 )
+{ 
+    alert("É noite");
+}
+else if (horas <= 18)
+{ 
+    alert("É dia");
+}
